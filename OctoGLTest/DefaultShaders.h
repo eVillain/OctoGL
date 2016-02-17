29809aab0f04e4ee800501762dc6b469e1ea0a08
eVillain <eVillain@eVillAir.local>
2016-02-17 12:49:11 +0100

    WTF git.

--- OctoGLTest/DefaultShaders.h
+++ OctoGLTest/DefaultShaders.h
@@ -1,13 +1,18 @@
-//
-//  DefaultShaders.h
-//  OctoGL
-//
-//  Created by eVillain on 17/02/16.
-//  Copyright © 2016 The Drudgerist. All rights reserved.
-//
-
 #ifndef DefaultShaders_h
 #define DefaultShaders_h
 
+const char* vertex_shader =
+"#version 400\n"
+"in vec3 vp;"
+"void main () {"
+"  gl_Position = vec4 (vp, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 400\n"
+"out vec4 frag_colour;"
+"void main () {"
+"  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+"}";
 
 #endif /* DefaultShaders_h */
